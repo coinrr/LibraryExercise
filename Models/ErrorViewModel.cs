18659a4492900cb1e7540f75aeacdbc952915443
coinrr <rr@coinrr.io>
2018-07-09 16:16:30 +0800

    Initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LibraryExercise.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
